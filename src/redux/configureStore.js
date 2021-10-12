2544f10dc781f65389a0a3e030d1ac9a089ebe06
Daniel Samuel <dansam5000@gmail.com>
2021-10-12 20:48:54 +0100

    mod: Modify configure store importing depencies

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
-import covidDataReducer from './covidData/covidDatas';
+import covidDataReducer from './covidData/covidData';
 const store = createStore(covidDataReducer, applyMiddleware(thunk, logger));
 
 export default store;
