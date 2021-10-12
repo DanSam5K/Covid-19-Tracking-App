cad1ab81d0a3ba3720abfb2d0b94d9d4708a5661
Daniel Samuel <dansam5000@gmail.com>
2021-10-12 12:37:32 +0100

    feat: Build skeleton home and details component feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Details from './components/Details';
+import Home from './components/Home';
 import '../src/assets/css/App.css';
 
-function App() {}
+function App() {
+  <>
+    <Home>Home section</Home>
+    <Details>Details section</Details>
+  </>;
+}
 
 export default App;
