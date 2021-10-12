6421ec044da82f0deac1f79445c5217035715c93
Daniel Samuel <dansam5000@gmail.com>
2021-10-12 20:47:54 +0100

    feat: Implement app fdisplay features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone, faCog } from '@fortawesome/react-fontawesome';
+import { Navbar } from 'react-bootstrap';
+import { Link, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Details from './components/Details';
 import Home from './components/Home';
 import '../src/assets/css/App.css';
 
 function App() {
-  <>
-    <Home>Home section</Home>
-    <Details>Details section</Details>
-  </>;
+  return (
+    <>
+      <header>
+        <Navbar className="px-4 text-white bg-blue-dark d-flex justify-content-between">
+          <Link to="/" className="text-decoration-none text-white fw-bold">
+            &#60; HOME
+          </Link>
+          <h3 className="m-0">COVID-19 CASE STATS</h3>
+          <span>
+            <FontAwesomeIcon icon={faMicrophone} className="me-4" />
+            <FontAwesomeIcon icon={faCog} />
+          </span>
+        </Navbar>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </>
+  );
 }
 
 export default App;
