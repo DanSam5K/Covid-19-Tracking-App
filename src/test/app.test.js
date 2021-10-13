0112fa22db50686f5f325e29b0a7faff6af7aac9
Daniel Samuel <dansam5000@gmail.com>
2021-10-14 00:34:08 +0100

    test: Add test

---
+++ src/test/app.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/configureStore';
+import App from '../App';
+
+test('Should display the header', () => {
+  render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  );
+  const headerText = screen.getByText(/COVID-19 CASE STATS/i);
+  expect(headerText).toBeInTheDocument();
+});
