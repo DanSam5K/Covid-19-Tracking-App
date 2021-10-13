0290fa55b4d6bc586480417a5609f821b5030392
Daniel Samuel <dansam5000@gmail.com>
2021-10-13 19:18:05 +0100

    feat: Dispatch action on store to fetch data from API

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { fetchData } from './dataApi/covidDataApi';
 import store from './redux/configureStore';
 import './assets/css/index.css';
 import App from './App';
+
+store.dispatch(fetchData);
 
 ReactDOM.render(
   <React.StrictMode>
